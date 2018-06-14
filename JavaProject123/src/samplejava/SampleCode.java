a6668f08db31654ea8190a65736d9836a177dd82
ramu9934 <ramunakkana.90@gmail.com>
2018-06-14 12:25:10 +0530

    Revert "added new line"
    This reverts commit 36397d9e44d5c1335aec126a458e66b19db61fac.

--- JavaProject123/src/samplejava/SampleCode.java
+++ JavaProject123/src/samplejava/SampleCode.java
@@ -5,7 +5,6 @@ public class SampleCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				System.out.println("hii");
-				System.out.println("hello java");
 	}
 
 }
