605459200c44f2c62e9d647ad4a3e8c607b0b41b
ramu9934 <ramunakkana.90@gmail.com>
2018-06-19 19:12:30 +0530

    hiiii

---
+++ JavaProject123/src/samplejava/LoopConcept.java
@@ -0,0 +1,20 @@
+package samplejava;
+
+public class LoopConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 //forloop 
+		for(int i=1;i<=10;i++){ //initalization,conditional
+			System.out.println(i);
+		}
+			
+			//to print 10 t0 1
+			System.out.println("...........");
+			for(int k=10;k>=1;k--){
+				System.out.println(k);
+			}
+		} 
+		}
+	
+
